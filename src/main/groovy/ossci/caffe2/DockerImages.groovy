36763a17554e72750c6cbcad7faef231c8aec4a5
pjh5 <hellemn@fb.com>
2018-05-08 10:01:59 -0700

    Disabling all conda builds

--- src/main/groovy/ossci/caffe2/DockerImages.groovy
+++ src/main/groovy/ossci/caffe2/DockerImages.groovy
@@ -61,11 +61,7 @@ class DockerImages {
     'conda3-cuda8.0-cudnn7-ubuntu16.04',
 
     'conda2-gcc4.8-ubuntu16.04',
-    //'conda2-cuda9.0-cudnn7-gcc4.8-ubuntu16.04',
-    //'conda2-cuda8.0-cudnn7-gcc4.8-ubuntu16.04',
     'conda3-gcc4.8-ubuntu16.04',
-    //'conda3-cuda9.0-cudnn7-gcc4.8-ubuntu16.04',
-    //'conda3-cuda8.0-cudnn7-gcc4.8-ubuntu16.04',
 
     'conda2-integrated-ubuntu16.04',
     'conda2-cuda8.0-cudnn7-integrated-ubuntu16.04',
@@ -73,8 +69,6 @@ class DockerImages {
 
     'conda3-cuda8.0-cudnn7-integrated-slim-ubuntu16.04',
     'conda3-cuda9.0-cudnn7-integrated-slim-ubuntu16.04',
-    //'conda3-cuda8.0-cudnn7-gcc4.8-integrated-slim-ubuntu16.04',
-    //'conda3-cuda9.0-cudnn7-gcc4.8-integrated-slim-ubuntu16.04',
 
     // CentOS images (Python 2 only)
     'py2-centos7',

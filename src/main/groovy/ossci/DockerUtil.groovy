f898b366090a36452c7b4bea16c6f9725434eac8
Junjie Bai <bai@in.tum.de>
2018-06-15 13:56:43 -0700

    add amd special docker flags

--- src/main/groovy/ossci/DockerUtil.groovy
+++ src/main/groovy/ossci/DockerUtil.groovy
@@ -114,6 +114,10 @@ if [ -n "${CPU_PERF_TEST:-}" ] && [[ $(/bin/hostname) == *packet* ]]; then
   docker_args+=" --security-opt seccomp=/var/lib/jenkins/allow_perf_event_open.json"
 fi
 
+if [ "${BUILD_ENVIRONMENT}" == *-rocm* ]]; then
+  docker_args+=" --device=/dev/kfd --device=/dev/dri --group-add video"
+fi
+
 # Image
 docker_args+=" ${DOCKER_IMAGE}"
 

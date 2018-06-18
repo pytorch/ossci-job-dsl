23ed3115f6cc1d592b22db3b4368bacf21cc6acf
Will Feng <willfeng@fb.com>
2018-06-17 20:38:34 -0400

    Add CUDA 9.2 GCC 7.3 job

--- src/jobs/pytorch.groovy
+++ src/jobs/pytorch.groovy
@@ -26,6 +26,7 @@ def buildEnvironments = [
   "pytorch-linux-xenial-cuda8-cudnn6-py3",
   "pytorch-linux-xenial-cuda9-cudnn7-py2",
   "pytorch-linux-xenial-cuda9-cudnn7-py3",
+  "pytorch-linux-xenial-cuda9.2-cudnn7-py3-gcc7.3",
   "pytorch-linux-xenial-py3-clang5-asan",
   "pytorch-linux-trusty-py2.7.9",
   "pytorch-linux-trusty-py2.7",

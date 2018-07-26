c4a1eff131721dcd3bc710c061b17ec6d6345c1b
Edward Z. Yang <ezyang@fb.com>
2018-07-25 21:14:15 -0400

    Revert "disable rocm job"
    This reverts commit 81b714e850b56695aa8f56c3d0f82483fb8da361.

--- src/jobs/pytorch.groovy
+++ src/jobs/pytorch.groovy
@@ -40,7 +40,7 @@ def buildEnvironments = [
   "pytorch-macos-10.13-cuda9.2-cudnn7-py3",
 
   // NB: This image is taken from Caffe2
-  // "py2-clang3.8-rocm1.7.1-ubuntu16.04",
+  "py2-clang3.8-rocm1.7.1-ubuntu16.04",
 
   // This is really expensive to run because it is a total build
   // from scratch.  Maybe we have to do it nightly.

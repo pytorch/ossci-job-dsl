1da6ecbaa9df22fade93ef463cd915008c3c10d5
pjh5 <hellemn@fb.com>
2018-09-07 00:22:29 -0700

    Fixes

--- src/jobs/packages.groovy
+++ src/jobs/packages.groovy
@@ -256,6 +256,10 @@ desired_python="${DESIRED_PYTHON:2:1}.${DESIRED_PYTHON:3:1}"
 # Reinitialize path (see man page for path_helper(8))
 eval `/usr/libexec/path_helper -s`
 
+# Coordinate one output folder across scripts
+export WHEEL_FINAL_FOLDER="$(pwd)/final_wheel"
+mkdir -p "$WHEEL_FINAL_FOLDER"
+
 # Build the wheel
 ./wheel/build_wheel.sh "\\$desired_python" "\\$PYTORCH_BUILD_VERSION" "\\$PYTORCH_BUILD_NUMBER"
 

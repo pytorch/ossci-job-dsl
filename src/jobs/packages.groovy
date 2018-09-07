70fccac98e4ce3befb4e6d3db92968c2d20dda19
pjh5 <hellemn@fb.com>
2018-09-06 21:26:30 -0700

    Fix to mac upload

--- src/jobs/packages.groovy
+++ src/jobs/packages.groovy
@@ -264,7 +264,7 @@ if [[ "$UPLOAD_PACKAGE" == true ]]; then
   export PATH=$(pwd)/tmp_conda/bin:$PATH
   conda create -yn aws36 python=3.6
   source activate aws36
-  pip install aws-cli
+  pip install awscli
   pushd ./wheel
   ./upload.sh
   popd

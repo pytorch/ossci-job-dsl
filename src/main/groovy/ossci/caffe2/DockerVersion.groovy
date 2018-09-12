f28878d195bf66e26da0acbb2b6ea83f820c31b1
Edward Z. Yang <ezyang@fb.com>
2018-09-11 22:56:59 -0400

    Revert "Update Caffe2 DockerVersion"
    This reverts commit 63d8ebf8bbd3607cbef4c3e80b87fd66be542b1b.

--- src/main/groovy/ossci/caffe2/DockerVersion.groovy
+++ src/main/groovy/ossci/caffe2/DockerVersion.groovy
@@ -2,5 +2,5 @@
 // <DockerVersion>190</DockerVersion>  # This line is needed by CircleCI builds, please don't remove it
 package ossci.caffe2
 class DockerVersion {
-  static final String version = "194";
+  static final String version = "190";
 }

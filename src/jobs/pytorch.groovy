4adc4072f54cab3332637c1620c455da3857ea7b
Edward Z. Yang <ezyang@fb.com>
2018-09-26 15:28:33 -0400

    bump windows timeout
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- src/jobs/pytorch.groovy
+++ src/jobs/pytorch.groovy
@@ -995,7 +995,7 @@ fi
     job("${buildBasePath}/${buildEnvironment}-build") {
       JobUtil.common delegate, 'windows && cpu'
       JobUtil.gitCommitFromPublicGitHub delegate, '${GITHUB_REPO}'
-      JobUtil.timeoutAndFailAfter(delegate, 90)
+      JobUtil.timeoutAndFailAfter(delegate, 120)
       // Windows builds are something like 9M a pop, so keep less of
       // them.
       publishers {

10751014c16a0a2a34dccaa1cb54fe6fbeea3ba8
Junjie Bai <bai@in.tum.de>
2018-11-30 22:21:11 -0800

    Revert "Turn off rocm jobs in pull requests until amd data center outage is recovered"
    This reverts commit ea2cd093020e2d62b80381e28e75fcc190d15e91.

--- src/jobs/pytorch.groovy
+++ src/jobs/pytorch.groovy
@@ -211,11 +211,6 @@ def pullRequestJobSettings = { context, repo, commitSource ->
               predefinedProp('GITHUB_REPO', repo)
               // Ensure consistent merge behavior in downstream builds.
               propertiesFile(gitPropertiesFile)
-              // Disable ROCM tests on pytorch/pytorch PRs; there is too
-              // much volume for three test machines to handle
-              if (isRocmBuild(buildEnvironment) && commitSource == "pull-request") {
-                booleanParam('RUN_TESTS', false)
-              }
             }
           }
         }
@@ -233,11 +228,6 @@ def pullRequestJobSettings = { context, repo, commitSource ->
               predefinedProp('ghprbCredentialsId', pytorchbotAuthId)
               // Ensure consistent merge behavior in downstream builds.
               propertiesFile(gitPropertiesFile)
-              // Disable ROCM tests on pytorch/pytorch PRs; there is too
-              // much volume for three test machines to handle
-              if (isRocmBuild(name) && commitSource == "pull-request") {
-                booleanParam('RUN_TESTS', false)
-              }
             }
           }
         }

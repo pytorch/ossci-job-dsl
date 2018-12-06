b2784093fc6f550fbd6be845278d503ef4e20569
Junjie Bai <bai@in.tum.de>
2018-12-05 22:24:45 -0800

    Bump caffe2 docker images verison to 238

--- src/main/groovy/ossci/caffe2/DockerVersion.groovy
+++ src/main/groovy/ossci/caffe2/DockerVersion.groovy
@@ -1,6 +1,6 @@
 // This file is manually managed
 package ossci.caffe2
 class DockerVersion {
-  static final String version = "230";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
-  static final String allDeployedVersions = "230,213";  // NOTE: this is a comma-separated list. E.g. "262,220,219"
+  static final String version = "238";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
+  static final String allDeployedVersions = "238,230,213";  // NOTE: this is a comma-separated list. E.g. "262,220,219"
 }

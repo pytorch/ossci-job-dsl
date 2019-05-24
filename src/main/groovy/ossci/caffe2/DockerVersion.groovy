21a85fde037207555033fc0180e26466dc90aade
Roman Dzhabarov <34466929+rdzhabarov@users.noreply.github.com>
2019-05-24 13:33:02 -0700

    [docker] Update docker image to the latest
    See the run here: https://ci.pytorch.org/jenkins/job/caffe2-docker-trigger/281/

--- src/main/groovy/ossci/caffe2/DockerVersion.groovy
+++ src/main/groovy/ossci/caffe2/DockerVersion.groovy
@@ -1,6 +1,6 @@
 // This file is manually managed
 package ossci.caffe2
 class DockerVersion {
-  static final String version = "276";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
-  static final String allDeployedVersions = "276,273,266,253,248,238,230,213";  // NOTE: this is a comma-separated list. E.g. "262,220,219"
+  static final String version = "281";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
+  static final String allDeployedVersions = "281,276,273,266,253,248,238,230,213";  // NOTE: this is a comma-separated list. E.g. "262,220,219"
 }

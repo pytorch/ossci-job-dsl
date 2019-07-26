94f1700e3db3765c01568aaa7e01103d2cc86dc4
Edward Z. Yang <ezyang@fb.com>
2019-07-26 16:41:48 -0400

    Bump up deployed version
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- src/main/groovy/ossci/pytorch/DockerVersion.groovy
+++ src/main/groovy/ossci/pytorch/DockerVersion.groovy
@@ -2,5 +2,5 @@
 package ossci.pytorch
 class DockerVersion {
   static final String version = "300";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
-  static final String allDeployedVersions = "271,262,256,278,282,291,300,321";  // NOTE: this is a comma-separated list. E.g. "262,220,219"
+  static final String allDeployedVersions = "271,262,256,278,282,291,300,323";  // NOTE: this is a comma-separated list. E.g. "262,220,219"
 }

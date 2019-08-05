e1e4e8440b2269dfcd290089c1c331452c36a900
Edward Z. Yang <ezyang@fb.com>
2019-08-05 12:00:45 -0400

    Save 327 which adds cuda 10.1
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- src/main/groovy/ossci/pytorch/DockerVersion.groovy
+++ src/main/groovy/ossci/pytorch/DockerVersion.groovy
@@ -1,6 +1,10 @@
 // This file is manually managed
 package ossci.pytorch
 class DockerVersion {
-  static final String version = "300";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
-  static final String allDeployedVersions = "271,262,256,278,282,291,300,323";  // NOTE: this is a comma-separated list. E.g. "262,220,219"
+  // WARNING: if you change this to a new version number,
+  // you **MUST** also add that version number to the allDeployedVersions list below
+  static final String version = "323";
+
+  // NOTE: this is a comma-separated list. E.g. "262,220,219"
+  static final String allDeployedVersions = "271,262,256,278,282,291,300,323,327";
 }

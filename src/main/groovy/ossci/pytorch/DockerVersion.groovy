5dbc0c7cf0c07d56571e00069a52724da08a0936
Edward Z. Yang <ezyang@fb.com>
2019-11-11 11:05:53 -0500

    add new docker version to save
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- src/main/groovy/ossci/pytorch/DockerVersion.groovy
+++ src/main/groovy/ossci/pytorch/DockerVersion.groovy
@@ -3,8 +3,8 @@ package ossci.pytorch
 class DockerVersion {
   // WARNING: if you change this to a new version number,
   // you **MUST** also add that version number to the allDeployedVersions list below
-  static final String version = "347";
+  static final String version = "383";
 
   // NOTE: this is a comma-separated list. E.g. "262,220,219"
-  static final String allDeployedVersions = "271,262,256,278,282,291,300,323,327,347";
+  static final String allDeployedVersions = "271,262,256,278,282,291,300,323,327,347,383";
 }

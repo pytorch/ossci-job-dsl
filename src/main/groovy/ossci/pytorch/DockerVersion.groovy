e160bd2c3ddc1124fa4f6c4a4c4022160eb2abe0
IvanKobzarev <ivankobzarev@fb.com>
2019-11-12 15:43:20 -0800

    bump pytorch docker image version to 403

--- src/main/groovy/ossci/pytorch/DockerVersion.groovy
+++ src/main/groovy/ossci/pytorch/DockerVersion.groovy
@@ -3,8 +3,8 @@ package ossci.pytorch
 class DockerVersion {
   // WARNING: if you change this to a new version number,
   // you **MUST** also add that version number to the allDeployedVersions list below
-  static final String version = "402";
+  static final String version = "403";
 
   // NOTE: this is a comma-separated list. E.g. "262,220,219"
-  static final String allDeployedVersions = "271,262,256,278,282,291,300,323,327,347,383,389,401,402";
+  static final String allDeployedVersions = "271,262,256,278,282,291,300,323,327,347,383,389,401,402,403";
 }

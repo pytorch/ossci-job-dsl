94a5021aed35db4517eac115b09b9c6e3c53708d
Edward Z. Yang <ezyang@fb.com>
2019-11-13 10:15:09 -0500

    Delete 383 it was never deployed
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- src/main/groovy/ossci/pytorch/DockerVersion.groovy
+++ src/main/groovy/ossci/pytorch/DockerVersion.groovy
@@ -6,5 +6,5 @@ class DockerVersion {
   static final String version = "405";
 
   // NOTE: this is a comma-separated list. E.g. "262,220,219"
-  static final String allDeployedVersions = "271,262,256,278,282,291,300,323,327,347,383,389,401,402,403,405";
+  static final String allDeployedVersions = "271,262,256,278,282,291,300,323,327,347,389,401,402,403,405";
 }

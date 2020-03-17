546797c9378fe3ba572531ab9711780db85b88ac
Edward Z. Yang <ezyang@fb.com>
2020-03-17 12:24:56 -0400

    amd is the only thing left
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- src/main/groovy/ossci/caffe2/Images.groovy
+++ src/main/groovy/ossci/caffe2/Images.groovy
@@ -11,76 +11,76 @@ class Images {
   //   baseImageOf<jenkinsBuildName, dockerImageName>
   // that is defined below
   static final List<String> dockerBaseImages = [
-    // Primary builds
-    'py2-cuda8.0-cudnn7-ubuntu16.04',
-    'py2-cuda9.0-cudnn7-ubuntu16.04',
-    'py2-cuda9.1-cudnn7-ubuntu16.04',
-    'py2-cuda10.1-cudnn7-ubuntu16.04',
-    'py2-gcc5-ubuntu16.04',
-    'py2-mkl-ubuntu16.04',
+    //// Primary builds
+    //'py2-cuda8.0-cudnn7-ubuntu16.04',
+    //'py2-cuda9.0-cudnn7-ubuntu16.04',
+    //'py2-cuda9.1-cudnn7-ubuntu16.04',
+    //'py2-cuda10.1-cudnn7-ubuntu16.04',
+    //'py2-gcc5-ubuntu16.04',
+    //'py2-mkl-ubuntu16.04',
 
-    // Primary builds (Python 3.5, available by default on Ubuntu)
-    'py3.5-cuda8.0-cudnn7-ubuntu16.04',
-    'py3.5-cuda9.0-cudnn7-ubuntu16.04',
-    'py3.5-cuda9.1-cudnn7-ubuntu16.04',
-    'py3.5-cuda10.1-cudnn7-ubuntu16.04',
-    'py3.5-gcc5-ubuntu16.04',
-    'py3.5-mkl-ubuntu16.04',
+    //// Primary builds (Python 3.5, available by default on Ubuntu)
+    //'py3.5-cuda8.0-cudnn7-ubuntu16.04',
+    //'py3.5-cuda9.0-cudnn7-ubuntu16.04',
+    //'py3.5-cuda9.1-cudnn7-ubuntu16.04',
+    //'py3.5-cuda10.1-cudnn7-ubuntu16.04',
+    //'py3.5-gcc5-ubuntu16.04',
+    //'py3.5-mkl-ubuntu16.04',
 
-    // Python compatibility (Python 3.6 is not default)
-    'py3.6-gcc5-ubuntu16.04',
-    'py3.6-clang6.0-ubuntu16.04',
-    'py3.6-clang7-ubuntu16.04',
+    //// Python compatibility (Python 3.6 is not default)
+    //'py3.6-gcc5-ubuntu16.04',
+    //'py3.6-clang6.0-ubuntu16.04',
+    //'py3.6-clang7-ubuntu16.04',
 
-    // Compatibility check for cuDNN 6 and 5 (build only)
-    'py2-cuda8.0-cudnn6-ubuntu16.04',
-    // EOLed on 2018-06-26 by Yangqing, Pieter, Mingzhe
-    // 'py2-cuda8.0-cudnn5-ubuntu16.04',
+    //// Compatibility check for cuDNN 6 and 5 (build only)
+    //'py2-cuda8.0-cudnn6-ubuntu16.04',
+    //// EOLed on 2018-06-26 by Yangqing, Pieter, Mingzhe
+    //// 'py2-cuda8.0-cudnn5-ubuntu16.04',
 
-    // Compiler compatibility for 14.04 (build only)
-    'py2-gcc4.8-ubuntu14.04',
-    'py2-gcc4.9-ubuntu14.04',
+    //// Compiler compatibility for 14.04 (build only)
+    //'py2-gcc4.8-ubuntu14.04',
+    //'py2-gcc4.9-ubuntu14.04',
 
-    // Compiler compatibility for 16.04 (build only)
-    'py2-gcc6-ubuntu16.04',
-    'py2-gcc7-ubuntu16.04',
-    'py2-clang3.8-ubuntu16.04',
-    'py2-clang3.9-ubuntu16.04',
-    'py2-clang4.0-ubuntu16.04',
-    'py2-clang5.0-ubuntu16.04',
-    'py2-clang6.0-ubuntu16.04',
-    'py2-clang7-ubuntu16.04',
-    'py2-clang8-ubuntu16.04',
+    //// Compiler compatibility for 16.04 (build only)
+    //'py2-gcc6-ubuntu16.04',
+    //'py2-gcc7-ubuntu16.04',
+    //'py2-clang3.8-ubuntu16.04',
+    //'py2-clang3.9-ubuntu16.04',
+    //'py2-clang4.0-ubuntu16.04',
+    //'py2-clang5.0-ubuntu16.04',
+    //'py2-clang6.0-ubuntu16.04',
+    //'py2-clang7-ubuntu16.04',
+    //'py2-clang8-ubuntu16.04',
 
-    // Python 3 compiler compatibility
-    'py3.5-gcc6-ubuntu16.04',
-    'py3.5-gcc7-ubuntu16.04',
-    'py3.5-clang3.8-ubuntu16.04',
-    'py3.5-clang3.9-ubuntu16.04',
-    'py3.5-clang4.0-ubuntu16.04',
-    'py3.5-clang5.0-ubuntu16.04',
+    //// Python 3 compiler compatibility
+    //'py3.5-gcc6-ubuntu16.04',
+    //'py3.5-gcc7-ubuntu16.04',
+    //'py3.5-clang3.8-ubuntu16.04',
+    //'py3.5-clang3.9-ubuntu16.04',
+    //'py3.5-clang4.0-ubuntu16.04',
+    //'py3.5-clang5.0-ubuntu16.04',
 
-    // Build for Android
-    'py2-android-ubuntu16.04',
-    'py2-android-ndk-r19c-ubuntu16.04',
+    //// Build for Android
+    //'py2-android-ubuntu16.04',
+    //'py2-android-ndk-r19c-ubuntu16.04',
 
-    // Builds for Anaconda
-    'conda2-ubuntu16.04',
-    'conda2-cuda10.1-cudnn7-ubuntu16.04',
-    'conda2-cuda9.0-cudnn7-ubuntu16.04',
-    'conda2-cuda8.0-cudnn7-ubuntu16.04',
-    'conda3-ubuntu16.04',
-    'conda3-cuda10.1-cudnn7-ubuntu16.04',
-    'conda3-cuda9.0-cudnn7-ubuntu16.04',
-    'conda3-cuda8.0-cudnn7-ubuntu16.04',
+    //// Builds for Anaconda
+    //'conda2-ubuntu16.04',
+    //'conda2-cuda10.1-cudnn7-ubuntu16.04',
+    //'conda2-cuda9.0-cudnn7-ubuntu16.04',
+    //'conda2-cuda8.0-cudnn7-ubuntu16.04',
+    //'conda3-ubuntu16.04',
+    //'conda3-cuda10.1-cudnn7-ubuntu16.04',
+    //'conda3-cuda9.0-cudnn7-ubuntu16.04',
+    //'conda3-cuda8.0-cudnn7-ubuntu16.04',
 
-    'conda2-gcc4.8-ubuntu16.04',
-    'conda3-gcc4.8-ubuntu16.04',
+    //'conda2-gcc4.8-ubuntu16.04',
+    //'conda3-gcc4.8-ubuntu16.04',
 
-    // CentOS images
-    'py3.6-devtoolset7-centos7',
-    'py3.6-devtoolset7-cuda9.2-cudnn7-centos7',
-    'py3.6-devtoolset7-cuda10.1-cudnn7-centos7',
+    //// CentOS images
+    //'py3.6-devtoolset7-centos7',
+    //'py3.6-devtoolset7-cuda9.2-cudnn7-centos7',
+    //'py3.6-devtoolset7-cuda10.1-cudnn7-centos7',
 
      // AMD ROCM builds
     'py3.6-clang7-rocmdeb-ubuntu16.04',

8a2cf0b810f16574b59b5dbf1d44bf3633937869
Edward Z. Yang <ezyang@fb.com>
2020-03-26 10:49:14 -0400

    Bump image version
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- src/main/groovy/ossci/caffe2/DockerVersion.groovy
+++ src/main/groovy/ossci/caffe2/DockerVersion.groovy
@@ -1,6 +1,6 @@
 // This file is manually managed
 package ossci.caffe2
 class DockerVersion {
-  static final String version = "345";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
-  static final String allDeployedVersions = "348,345,336,325,324,315,306,301,287,283,276,273,266,253,248,238,230,213";  // NOTE: this is a comma-separated list. E.g. "262,220,219"
+  static final String version = "369";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
+  static final String allDeployedVersions = "369,348,345,336,325,324,315,306,301,287,283,276,273,266,253,248,238,230,213";  // NOTE: this is a comma-separated list. E.g. "262,220,219"
 }

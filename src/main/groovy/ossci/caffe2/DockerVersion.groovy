d2f3faf287959df986c59af872fe0daf0030a428
Jeff Daily <jeff.daily@amd.com>
2020-06-11 08:17:28 -0700

    Revert "use circleci rocm images (#67)"
    This reverts commit d5efdbe887a386a01b5639517950039d337de234.

--- src/main/groovy/ossci/caffe2/DockerVersion.groovy
+++ src/main/groovy/ossci/caffe2/DockerVersion.groovy
@@ -1,6 +1,6 @@
 // This file is manually managed
 package ossci.caffe2
 class DockerVersion {
-  static final String version = "be76e8fd-44e2-484d-b090-07e0cc3a56f0";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
+  static final String version = "376";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
   static final String allDeployedVersions = "376,373,348,345,336,325,324,315,306,301,287,283,276,273,266,253,248,238,230,213";  // NOTE: this is a comma-separated list. E.g. "262,220,219"
 }
--- src/main/groovy/ossci/pytorch/DockerVersion.groovy
+++ src/main/groovy/ossci/pytorch/DockerVersion.groovy
@@ -7,7 +7,7 @@ package ossci.pytorch
 class DockerVersion {
   // WARNING: if you change this to a new version number,
   // you **MUST** also add that version number to the allDeployedVersions list below
-  static final String version = "be76e8fd-44e2-484d-b090-07e0cc3a56f0";
+  static final String version = "405";
 
   // NOTE: this is a comma-separated list. E.g. "262,220,219"
   static final String allDeployedVersions = "271,262,256,278,282,291,300,323,327,347,389,401,402,403,405";

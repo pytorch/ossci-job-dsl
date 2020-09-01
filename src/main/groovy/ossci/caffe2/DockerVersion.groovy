9272d0dffe50a39790efbdc797a9e8eb4123c4d5
Jeff Daily <jeff.daily@amd.com>
2020-09-01 08:30:49 -0700

    upgrade rocm image to 3.7 (#76)

--- src/main/groovy/ossci/caffe2/DockerVersion.groovy
+++ src/main/groovy/ossci/caffe2/DockerVersion.groovy
@@ -1,6 +1,6 @@
 // This file is manually managed
 package ossci.caffe2
 class DockerVersion {
-  static final String version = "ab1632df-fa59-40e6-8c23-98e004f61148";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
+  static final String version = "05efe7b7cf0d2b143b09ce3d9ab0fc6724e397ce";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
   static final String allDeployedVersions = "376,373,348,345,336,325,324,315,306,301,287,283,276,273,266,253,248,238,230,213";  // NOTE: this is a comma-separated list. E.g. "262,220,219"
 }

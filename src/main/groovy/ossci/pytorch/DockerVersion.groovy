d31e01a3496985ed50e1124a3d817e1e2e002ca3
Eli Uriegas <eliuriegas@fb.com>
2020-09-03 09:01:37 -0700

    bump dockerversion
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- src/main/groovy/ossci/pytorch/DockerVersion.groovy
+++ src/main/groovy/ossci/pytorch/DockerVersion.groovy
@@ -7,7 +7,7 @@ package ossci.pytorch
 class DockerVersion {
   // WARNING: if you change this to a new version number,
   // you **MUST** also add that version number to the allDeployedVersions list below
-  static final String version = "05efe7b7cf0d2b143b09ce3d9ab0fc6724e397ce";
+  static final String version = "1045c7b891104cb4fd23399eab413b6213e48aeb";
 
   // NOTE: this is a comma-separated list. E.g. "262,220,219"
   static final String allDeployedVersions = "271,262,256,278,282,291,300,323,327,347,389,401,402,403,405";

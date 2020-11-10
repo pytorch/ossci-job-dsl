1f1bf330d2ad09fa0c6aa09af3ca702fcd4b8ff2
Jane Xu <janeyx@fb.com>
2020-11-10 07:34:56 -0800

    updating hashes once again

--- src/main/groovy/ossci/pytorch/DockerVersion.groovy
+++ src/main/groovy/ossci/pytorch/DockerVersion.groovy
@@ -7,7 +7,7 @@ package ossci.pytorch
 class DockerVersion {
   // WARNING: if you change this to a new version number,
   // you **MUST** also add that version number to the allDeployedVersions list below
-  static final String version = "9e980b21bb85da4062fe89410f9d6af5c850fbca";
+  static final String version = "e30a450e520b68985457efd16da7003e495afe16";
 
   // NOTE: this is a comma-separated list. E.g. "262,220,219"
   static final String allDeployedVersions = "271,262,256,278,282,291,300,323,327,347,389,401,402,403,405";

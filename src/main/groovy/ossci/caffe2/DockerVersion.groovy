c8f4a0bc875a2ff5b50c2b45279b37b1a0e45054
Jeff Daily <jeff.daily@amd.com>
2021-02-02 14:54:36 -0800

    Revert "update ROCm CI image tag to latest, to pick up hipMAGMA support (#91)"
    This reverts commit 0ac806989337535388416f2b0649c5b6968d9924.

--- src/main/groovy/ossci/caffe2/DockerVersion.groovy
+++ src/main/groovy/ossci/caffe2/DockerVersion.groovy
@@ -1,6 +1,6 @@
 // This file is manually managed
 package ossci.caffe2
 class DockerVersion {
-  static final String version = "ba2ea059770948127484702300768ffd723e5cf9";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
+  static final String version = "584d64a27bdad2d14b0f897878f6c9d6b032a7e5";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
   static final String allDeployedVersions = "376,373,348,345,336,325,324,315,306,301,287,283,276,273,266,253,248,238,230,213";  // NOTE: this is a comma-separated list. E.g. "262,220,219"
 }

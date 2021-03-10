608e9899a9b3385ee6cbbca59a1b416c0952fa77
Jeff Daily <jeff.daily@amd.com>
2021-03-10 09:37:38 -0800

    update ROCm image tag to use latest with gcc9

--- src/main/groovy/ossci/caffe2/DockerVersion.groovy
+++ src/main/groovy/ossci/caffe2/DockerVersion.groovy
@@ -1,6 +1,6 @@
 // This file is manually managed
 package ossci.caffe2
 class DockerVersion {
-  static final String version = "1e738bfb344693aef5c596b1582e140b8347381c";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
+  static final String version = "f90df8e0edcb165bace6c48e23e5c27266f76638";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
   static final String allDeployedVersions = "376,373,348,345,336,325,324,315,306,301,287,283,276,273,266,253,248,238,230,213";  // NOTE: this is a comma-separated list. E.g. "262,220,219"
 }

8ba64ee9c748cfd4403da14b7ee6899c8604adfc
Jithun Nair <37884920+jithunnair-amd@users.noreply.github.com>
2021-05-27 17:22:17 -0500

    Update DockerVersion.groovy

--- src/main/groovy/ossci/caffe2/DockerVersion.groovy
+++ src/main/groovy/ossci/caffe2/DockerVersion.groovy
@@ -1,6 +1,6 @@
 // This file is manually managed
 package ossci.caffe2
 class DockerVersion {
-  static final String version = "cf0725f072e1d52d34c23d848a906fbc4dee44f6";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
+  static final String version = "2152ba6309b12251994fcdcd4ff234bfbe646d06";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
   static final String allDeployedVersions = "376,373,348,345,336,325,324,315,306,301,287,283,276,273,266,253,248,238,230,213";  // NOTE: this is a comma-separated list. E.g. "262,220,219"
 }

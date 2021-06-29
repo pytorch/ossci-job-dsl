41d9e8aab3c469a82fbc8cb97fe68ba1bcd5ab8c
Jeff Daily <jeff.daily@amd.com>
2021-06-29 07:16:39 -0700

    update docker image tag to tip of tree, pick up expecttest change (#108)

--- src/main/groovy/ossci/caffe2/DockerVersion.groovy
+++ src/main/groovy/ossci/caffe2/DockerVersion.groovy
@@ -1,6 +1,6 @@
 // This file is manually managed
 package ossci.caffe2
 class DockerVersion {
-  static final String version = "2152ba6309b12251994fcdcd4ff234bfbe646d06";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
+  static final String version = "bd62158e54445164d8748395febd095db42d268c";  // WARNING: if you change this to a new version number, you **MUST** also add that version number to the allDeployedVersions list below
   static final String allDeployedVersions = "376,373,348,345,336,325,324,315,306,301,287,283,276,273,266,253,248,238,230,213";  // NOTE: this is a comma-separated list. E.g. "262,220,219"
 }
--- src/main/groovy/ossci/pytorch/DockerVersion.groovy
+++ src/main/groovy/ossci/pytorch/DockerVersion.groovy
@@ -7,7 +7,7 @@ package ossci.pytorch
 class DockerVersion {
   // WARNING: if you change this to a new version number,
   // you **MUST** also add that version number to the allDeployedVersions list below
-  static final String version = "2152ba6309b12251994fcdcd4ff234bfbe646d06";
+  static final String version = "bd62158e54445164d8748395febd095db42d268c";
 
   // NOTE: this is a comma-separated list. E.g. "262,220,219"
   static final String allDeployedVersions = "271,262,256,278,282,291,300,323,327,347,389,401,402,403,405";

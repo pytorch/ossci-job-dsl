48518afe204327dc1d3b96578d89c0bffb01e17c
Jithun Nair <jithun.nair@amd.com>
2022-01-04 23:23:19 +0000

    Update docker image tags to py3.7

--- src/main/groovy/ossci/pytorch/DockerVersion.groovy
+++ src/main/groovy/ossci/pytorch/DockerVersion.groovy
@@ -7,7 +7,7 @@ package ossci.pytorch
 class DockerVersion {
   // WARNING: if you change this to a new version number,
   // you **MUST** also add that version number to the allDeployedVersions list below
-  static final String version = "fe94d108cf43defd99ac494eb346cf0b36d2219f";
+  static final String version = "f1c9c77c8e97b85a9aa45f51b157a8883ae52f2f";
 
   // NOTE: this is a comma-separated list. E.g. "262,220,219"
   static final String allDeployedVersions = "271,262,256,278,282,291,300,323,327,347,389,401,402,403,405";
